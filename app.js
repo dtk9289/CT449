9bc67d93e21473f0ebaf8f2f54df754a644eb2ac
kien <dtk9289@gmail.com>
2022-06-12 15:28:21 +0700

    cai dat xu ly loi

--- app.js
+++ app.js
@@ -1,15 +1,27 @@
 import express from "express"
 import cors from "cors"
 import contactRouter from "./app/routes/contact.route.js"
-
+import ApiError from "./app/api-error.js"
 const app = express()
 
 app.use(cors())
 app.use(express.json())
-app.use("/api/contacts", contactRouter)
 
 app.get("/", (req, res) => {
   res.json({ message: "Welcome to contact book application." })
+})
+
+app.use("/api/contacts", contactRouter)
+
+
+app.use((req, res, next) => {
+  return next(new ApiError(404, "Resource not found"))
+})
+
+app.use((err, req, res, next) => {
+  return res.status(err.statusCode || 500).json({
+    message: err.message || "Internal server error"
+  })
 })
 
 
