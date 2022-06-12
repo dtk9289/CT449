530e6af3318a1386c1dc302f5384c053f5995f1a
kien <dtk9289@gmail.com>
2022-06-12 15:13:35 +0700

    dinh nghia cac route cho tai nguyen contact

--- app.js
+++ app.js
@@ -1,13 +1,16 @@
 import express from "express"
 import cors from "cors"
+import contactRouter from "./app/routes/contact.route.js"
 
 const app = express()
 
 app.use(cors())
 app.use(express.json())
+app.use("/api/contacts", contactRouter)
 
-app.get("/", (req, res)=>{
-  res.json({message: "Welcome to contact book application."})
+app.get("/", (req, res) => {
+  res.json({ message: "Welcome to contact book application." })
 })
+
 
 export default app
---
+++ app/routes/contact.route.js
@@ -0,0 +1,18 @@
+import express from "express"
+import contactController from "../controller/contact.controller.js"
+const router = express.Router()
+
+router.route("/")
+  .get(contactController.findAll)
+  .post(contactController.create)
+  .delete(contactController.deleteAll)
+
+router.route("/favorite")
+  .get(contactController.findAllFavorite)
+
+router.route("/:id")
+  .get(contactController.findAllFavorite)
+  .put(contactController.update)
+  .delete(contactController.delete)
+
+export default router
